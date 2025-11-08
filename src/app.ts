15502fecdb322593f080f0eb8e70603ab2e497ba
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-11-08 22:41:47 +0100

    Initialization

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import AppError from "./utils/appError";
+import globalErrorHandler from "./controllers/errorController";
 
-const app:Application = express();
+const app: Application = express();
 
-app.use(cors({origin: "*"}))
-
-
-
+app.use(cors({ origin: "*" }));
 
 // Catch all unknown routes
 app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 export default app;
